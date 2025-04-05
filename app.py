0cb813707518bebf95af304637cc6dbea4c6798b
Kevin <72266546+kevin224860@users.noreply.github.com>
2025-04-04 23:28:57 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Hello from lastname ECS Container."
+    })
+
+if __name__ == "__main__":
+    # Listen on all interfaces so that it works in the Docker container
+    app.run(host="0.0.0.0", port=5000)
